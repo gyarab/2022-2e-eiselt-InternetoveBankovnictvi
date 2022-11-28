6aad4de1bbcbdc959418e14a07467fcf56a55b1f
Timon Eiselt <94890295+TimEiselt@users.noreply.github.com>
2022-11-28 20:32:54 +0100

    Update OvladaniPrihlaseni.java

--- internetovebankovnictvi/src/main/java/com/example/atm2/Ovladani/OvladaniPrihlaseni.java
+++ internetovebankovnictvi/src/main/java/com/example/atm2/Ovladani/OvladaniPrihlaseni.java
@@ -31,16 +31,4 @@ public class OvladaniPrihlaseni implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         prihlaseni_button.setOnAction(event -> Modely.getInstance().getNahled().showKlientOkno());
     }
-
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    public void prepniPrihlaseni(ActionEvent e) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Main.fxml")));
-        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        scene = new Scene(root, 1080, 720);
-        stage.setScene(scene);
-        stage.show();
-    }
 }
